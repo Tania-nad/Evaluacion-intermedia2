8623c43c2dd67f7ef535ef52379f9c4b82089db9
Tania <nadsanp@gmail.com>
2024-10-25 10:07:58 +0200

    pasos en humano

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 "use strict";
+
+/*
+  1- Se genera un número al azar entre el 1 y el 100
+  2- La jugadora introduce un número y da al botón "prueba"
+  3- Si el número que se recoge es mayor que el aleatorio
+        se muestra el mensaje: "demasiado alto" en la casilla de pista
+     Si el número es menor que el aleatorio
+        se muestra el mensaje "demasiado bajo"
+    Si el número es igual que el aleatorio
+        se muestra el mensaje "Has ganado, campeona!!"
+    Si se introduce un número mayor que 100
+        se muestra el mensaje: El número debe estar entre 1 y 100
+  4- Cuando se haga un intento, debe aumentar el número en la casilla de intentos
+  PASOS:
+  Recoger los elementos html:
+    input de número
+    botón de "prueba"
+    input de pista
+    input de numero de intentos
+  1
+  2
+  3
+  4
+  
+*/
+
+
+
