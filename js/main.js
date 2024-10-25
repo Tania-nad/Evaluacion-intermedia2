b122ff394ce235604bcaa7174b4965009c5c2e00
Tania <nadsanp@gmail.com>
2024-10-25 11:07:25 +0200

    final version non-finished

--- js/main.js
+++ js/main.js
@@ -1,29 +1,60 @@
 "use strict";
 
 /*
-  1- Se genera un número al azar entre el 1 y el 100
-  2- La jugadora introduce un número y da al botón "prueba"
-  3- Si el número que se recoge es mayor que el aleatorio
-        se muestra el mensaje: "demasiado alto" en la casilla de pista
-     Si el número es menor que el aleatorio
-        se muestra el mensaje "demasiado bajo"
-    Si el número es igual que el aleatorio
-        se muestra el mensaje "Has ganado, campeona!!"
-    Si se introduce un número mayor que 100
-        se muestra el mensaje: El número debe estar entre 1 y 100
-  4- Cuando se haga un intento, debe aumentar el número en la casilla de intentos
-  PASOS:
-  Recoger los elementos html:
+  
+  PASOS
+  - Recoger los elementos html:
     input de número
     botón de "prueba"
     input de pista
     input de numero de intentos
-  1
-  2
-  3
-  4
+  - Se genera un número al azar
+  - Si se introduce un número mayor que el aleatorio
+    aparece el mensaje: "demasiado alto"
+  - Si se introduce un número menor
+    aparece el mensaje: "demasiado bajo"
+
+  - Si el número es el mismo
+        aparece el mensaje: "Has ganado, campeona!!"
+  - Si se introduce un número mayor que 100
+        aparece el mensaje: "El número debe estar entre 1 y 100"
+  - Cuando se haga un intento, debe aumentar el número en la casilla de intentos
   
 */
+const number = document.querySelector(".js-number");
+const button = document.querySelector (".js-button");
+const clue = document.querySelector (".js-clue");
+const attempt = document.querySelector(".js-attempt");
+
+function getRandomNumber(max) { 
+    return Math.ceil(Math.random() * max); 
+  }
+
+  const randomNumber = getRandomNumber (100);
+  console.log(randomNumber);
+
+const handleClick = (ev) => {
+    ev.preventDefault();
+    console.log("click");
+    const numberUser = number.value;
+
+    if (numberUser > randomNumber ){
+        clue.innerHTML = "Demasiado alto";
+        } else if ( numberUser < randomNumber){
+            clue.innerHTML = "Demasiado bajo";
+
+
+
+    }
+    
+
+    
+}
+button.addEventListener ("click", handleClick);
+
+
+  
+
 
 
 
